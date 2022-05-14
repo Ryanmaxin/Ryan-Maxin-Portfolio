9fad3f8eadb2264172196e94d72066043b1968a8
Ryan Maxin <90675771+Ryanmaxin@users.noreply.github.com>
2022-05-14 19:21:28 -0400

    Basic layout of webiste created

---
+++ src/homePage/homePage.jsx
@@ -0,0 +1,13 @@
+import Nodes from "./nodes/nodes";
+import FacePlate from "./facePlate/facePlate";
+
+const HomePage = () => {
+  return (
+    <div id="homePage">
+      <Nodes />
+      <FacePlate />
+    </div>
+  );
+}
+
+export default HomePage;
