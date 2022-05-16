2623f0fee72ef102706d581062d32d56ef87bfff
Ryan Maxin <90675771+Ryanmaxin@users.noreply.github.com>
2022-05-16 17:30:34 -0400

    Final Change

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -16,6 +16,7 @@ const About = () => {
           An avid programmer since high school, I am passionate about creating websites and programs that look and feel great for users. The unique combination of creativity, logic, technology, and never running out of new things to discover drives my excitement and passion for software development. When I'm not programming or studying I like to spend my time keeping fit, socializing with friends, playing chess, or enjoying TV/movies.
         </p>
         <p>I primarily work with the <span class="emphasis">MERN</span> stack, however I am experienced with many other technologies, and always take the opportunity to learn new ones aswell. Take a look at my skills below.</p>
+        <p>P.S: Check out the page responsiveness!</p>
       </section>
       <section class="infotainer">
         <h4>Languages:</h4>
