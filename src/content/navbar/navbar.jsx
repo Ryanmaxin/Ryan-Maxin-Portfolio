49e5d8205c348f2a4510ca9ec333626a6fa3a71f
Ryan Maxin <rsmaxin@uwaterloo.ca>
2022-05-16 19:04:33 -0400

    FINAL

--- src/content/navbar/navbar.jsx
+++ src/content/navbar/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div id="navbar">
       <div class="btn-group" role="group" aria-label="Basic example">
-        <Link to="Ryan-Maxin-Portfolio" className="btn btn-outline-primary" id="left"
+        <Link to="/Ryan-Maxin-Portfolio" className="btn btn-outline-primary" id="left"
           role="button">Home</Link>
         <Link smooth to="/Ryan-Maxin-Portfolio/content#projects" className="btn btn-outline-primary" id="right"
           role="button">My Work</Link>
