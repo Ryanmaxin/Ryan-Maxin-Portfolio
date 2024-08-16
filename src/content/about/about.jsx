b7ae1fca6a47113d408d24c51c2fdb34b567a203
Ryan Maxin <rsmaxin@uwaterloo.ca>
2024-08-15 23:41:48 -0400

    Remaster about me content

--- src/content/about/about.jsx
+++ src/content/about/about.jsx
@@ -10,27 +10,30 @@ const About = () => {
           <h4>Greetings!</h4>
           <p>
             I am currently in my 3B term studying computer science at the
-            University of Waterloo, and I'm set to graduate in April 2026
+            University of Waterloo, where I'm set to graduate April of 2026
             &#127891;. Over the past three years, I've had the privilege of
             gaining hands-on experience through co-ops at four amazing
-            companies.
+            companies. It's at these companies I encountered the most
+            interesting and complex problems of my career so far, and where I
+            found my passion in coming up with clever solutions to solve them.
           </p>
           <p>
             I started my journey as a programmer when I took my first compsci
-            class in high school, where I instantly fell in love with the
-            discipline. The blend of creativity, logic, and the ever-evolving
-            landscape of technology fuels my excitement for software
-            development, and this is seen reflected in my track record of
-            success in co-op roles, personal projects, and academic courses.
+            class in high school, where I found myself fascinated by the
+            discipline. I found that the more I learned the more curious I got.
+            The blend of creativity, logic, and the ever-evolving landscape of
+            technology fuels my love for software development, and this is seen
+            reflected in my track record of success in co-op roles, personal
+            projects, and academic courses.
           </p>
           <p>
             I aim for a well rounded lifestyle. For me this primarily means
-            pursuing physical activity to contrast with my time programming.
-            Specifically I have gotten into the routine of weightlifting at the
-            gym 3-4 times a week. I also love to play soccer, badminton, ping
-            pong, and recently, I've also been challenging myself with rock
-            climbing. Outside of fitness, I enjoy playing chess and indulging in
-            some quality TV and movies.
+            pursuing physical activity to contrast with my time spent
+            programming. Most recently I have gotten into the routine of
+            weightlifting and have also been challenging myself with rock
+            climbing. Additionally I also love playing soccer, badminton, and
+            ping pong when I can. Outside of fitness, I enjoy playing chess and
+            indulging in some quality TV and movies.
           </p>
           <p>
             Looking forward, my goal is to secure a meaningful co-op position
